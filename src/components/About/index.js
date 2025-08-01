87f0506383602384fa3515518834a1014d893824
Shinra Zen <pratham18koli@gmail.com>
2025-08-01 10:33:56 +0530

    initial commit

---
+++ src/components/About/index.js
@@ -0,0 +1,74 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import { useEffect, useState } from 'react';
+import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+
+const About = () => {
+
+     const [letterClass, setLetterClass] = useState('text-animate');
+         useEffect(() => {
+     
+             const timer = setTimeout(() => {
+                 setLetterClass('text-animate-hover');
+             }, 4000);
+     
+     
+             return () => {
+                 clearTimeout(timer);
+             };
+         }, []);
+    return(
+        <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                    letterClass={letterClass}
+                        strArray={['A','b','o','u','t',' ','m','e']}
+                        idx={15}
+                    />
+                </h1>
+                <p>
+                    I'm a very ambitious front-end web developer looking for a role in established IT company with the opportunity to work with the latest technologies on challenging and diverse projects.
+                </p>
+                <p>
+                    I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+                </p>
+                <p>
+                    If I need to define myself in one sentence that would be I wield mind of a stoic and spirit of a warrior. Lifetime student and a tech enthusiast!
+
+                </p>
+            </div>
+            
+        <div className="stage-cube-content">
+            <div className="cubespinner">
+                <div className="face1">
+                    <FontAwesomeIcon icon={faHtml5} color="#DD0031"/>
+                </div>
+                <div className="face2">
+                    <FontAwesomeIcon icon={faCss3} color="#DD0031"/>
+                </div>
+                <div className="face3">
+                    <FontAwesomeIcon icon={faReact} color="#DD0031"/>
+                </div>
+                <div className="face4">
+                    <FontAwesomeIcon icon={faJsSquare} color="#DD0031"/>
+                </div>
+                <div className="face5">
+                    <FontAwesomeIcon icon={faGitAlt} color="#DD0031"/>
+                </div>
+                <div className="face6">
+                    <FontAwesomeIcon icon={faAngular} color="#EC4D28" />
+                </div>
+            </div>
+
+        </div>
+        </div>
+        <Loader type="pacman"/>
+        </>
+    )
+}
+
+export default About;
